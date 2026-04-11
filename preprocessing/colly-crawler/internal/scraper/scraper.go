d74bc23f185b34fd61d20675367ebb688e9c0582
AntonMartic <antma001@student.liu.se>
2026-04-12 00:07:42 +0200

    Start on scraper

--- preprocessing/colly-crawler/internal/scraper/scraper.go
+++ preprocessing/colly-crawler/internal/scraper/scraper.go
@@ -1,3 +1,42 @@
 package scraper
 
-//"github.com/gocolly/colly/v2"
+import (
+	"log"
+
+	"github.com/gocolly/colly/v2"
+)
+
+type Item struct {
+	Rank string
+	Name string
+	Score string
+}
+
+func ScrapeList(url string) ([]Item, error) {
+	var items []Item
+	c := newCollector()
+
+	c.OnHTML("h2", func(e *colly.HTMLElement) {
+		log.Print("found h2")
+	})
+
+	c.OnHTML("table tbody.ant-table-tbody tr.ant-table-row", func(e *colly.HTMLElement) {
+		item := Item{
+			Rank: e.ChildText("td:nth-child(1)"),
+			Name: e.ChildText("td:nth-child(2) a p"),
+			Score: e.ChildText("td:nth-child(3) span"),
+		}
+
+		items = append(items, item)
+	})
+
+	err := c.Visit(url)
+
+	log.Print("in scrape file")
+
+	return items, err
+}
+
+func newCollector() *colly.Collector {
+	return colly.NewCollector(colly.AllowedDomains("medieakademin.se"))
+}
--- preprocessing/colly-crawler/main.go
+++ preprocessing/colly-crawler/main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"colly-crawler/internal/scraper"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
 
+	//https://medieakademin.se/maktbarometern/
+
+	urls := [6]string{"https://medieakademin.se/maktbarometern-2025-facebook/", "https://medieakademin.se/maktbarometern-2025-instagram/", "https://medieakademin.se/maktbarometern-2025-youtube/", "https://medieakademin.se/maktbarometern-2025-x/", "https://medieakademin.se/maktbarometern-2025-tiktok/", "https://medieakademin.se/arets-makthavare-2025/"}
+
+	for _, url := range urls {
+		err := func() error {
+			parts := strings.Split(strings.Trim(url, "/"), "/")
+
+			filepath := "data/" + parts[len(parts)-1] + ".csv"
+
+			items, err := scraper.ScrapeList(url)
+
+			if err != nil {
+				return fmt.Errorf("Failed to scrape: %e", err)
+			}
+
+			file, err := os.Create(filepath)
+
+			if err != nil {
+				return fmt.Errorf("Failed to create file: %e", err)
+			}
+
+			defer file.Close()
+
+			writer := csv.NewWriter(file)
+
+			defer writer.Flush()
+
+			writer.Write([]string{"rank", "name", "score"})
+
+			for _, item := range items {
+				writer.Write([]string{item.Rank, item.Name, item.Score})
+			} 
+
+			fmt.Printf("Saved %d rows to %s\n", len(items), filepath)
+
+			return nil
+		}()
+
+		if err != nil {
+			log.Println(err)
+		}
+    }
 }
